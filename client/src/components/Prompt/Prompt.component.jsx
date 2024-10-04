93e271fa7a2ce0dbfd5c827a7b149fc23682ebeb
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2024-10-04 23:09:38 +0530

    Check Integrations

--- client/src/components/Prompt/Prompt.component.jsx
+++ client/src/components/Prompt/Prompt.component.jsx
@@ -2,7 +2,9 @@ import "./Prompt.Styles.scss"
 export const Prompt = ({text, type}) => {
     return (
         <div className={`prompt ${type}`}>
+            {/* {type == "Right-Prompt" && <p className="tag">V</p>} */}
             <p>{text}</p>
+            {/* {type == "left-Prompt" && <p className="tag">U</p>} */}
             </div>
     )
 }
